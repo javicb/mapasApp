81a61cabf48cdf1a6b1b58ba6f49bd1a901d40b9
Javi <javi.cb@gmail.com>
2021-02-23 13:57:31 +0100

    diseño zoomrange

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
+  styles: [`
+    .mapa-container {
+      height: 100%;
+      witdh: 100%;
+    }
+
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+  `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +27,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-4.42158411598413, 36.71954100713653],
+      zoom: 18
+    });
   }
 
 }
